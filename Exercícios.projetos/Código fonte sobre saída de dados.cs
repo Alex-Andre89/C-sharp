85fc3cf3c3ff8ef264bdb7608657e4a569f4b4d0
Alex André Souza Silva <alex.silvainf89@gmail.com>
2020-07-27 16:11:29 -0300

    Update

---
+++ Exercícios.projetos/Código fonte sobre saída de dados.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_código_fonte
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            double x = 10.35784;
+            int y = 32;
+            string z = "Maria";
+            char w = 'F';
+
+            Console.Write("Olá mundo!");
+            Console.WriteLine("Bom dia!");
+            Console.WriteLine("Até mais.");
+
+            Console.WriteLine();
+            Console.WriteLine(x);
+            Console.WriteLine(x.ToString("F2"));
+            Console.WriteLine(x.ToString("F4"));
+            Console.WriteLine(x.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.WriteLine();
+            Console.WriteLine("Resultado = " + x);
+            Console.WriteLine("O valor do troco é " + x + " reais ");
+            Console.WriteLine("O valor do troco é " + x.ToString("F2") + " reais ");
+
+            Console.WriteLine();
+            Console.WriteLine("A paciente " + z + " tem " + y + " anos e seu sexo é: " + w);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
